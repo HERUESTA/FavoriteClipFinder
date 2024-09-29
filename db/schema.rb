f769a624a0555820fcf231a8a434cc583f743f7e
seki <102042736+HERUESTA@users.noreply.github.com>
2024-09-29 19:42:06 +0900

    deviseの導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_28_111954) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_29_103316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.string "uid", default: "", null: false
+    t.string "user_name"
+    t.string "profile_image_url"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["uid"], name: "index_users_on_uid", unique: true
+  end
 end
