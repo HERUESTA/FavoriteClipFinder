97033c82dc0ca1c5796f77dda45cb05a038e759b
seki <102042736+HERUESTA@users.noreply.github.com>
2024-10-02 22:59:10 +0900

    Lint修正

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TwichClipFinder
     config.load_defaults 7.2
 
     config.session_store :cookie_store, key: "_twitch_clip_finder_session"
-    
+
     config.autoload_lib(ignore: %w[assets tasks])
   end
 end
