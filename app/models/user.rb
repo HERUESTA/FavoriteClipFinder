a569d5bc503f2c798eeca8eb910c78a894a10462
seki <102042736+HERUESTA@users.noreply.github.com>
2024-10-08 00:08:23 +0900

    トークンエラー

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,38 @@
-class UsersController < ApplicationController
-  def index
-    if current_user.present?
-      if current_user.refresh_token.blank?
-        Rails.logger.debug "リフレッシュトークンが存在しないため、再認証を実施します。"
-        redirect_to root_path 
-      end
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+         :omniauthable, omniauth_providers: [ :twitch ]
 
-      if current_user.token_expires_at.present? && current_user.token_expires_at < Time.now
-        Rails.logger.debug "アクセストークンの有効期限が切れています。再取得を試みます。"
-        refresh_access_token(current_user)
-      else
-        Rails.logger.debug "アクセストークンは有効です。"
-      end
-
-      user_id = current_user.id
-      @followed_channels = fetch_followed_channels(user_id)
-    else
-      Rails.logger.debug "ログインしているユーザーがいません。"
-      @followed_channels = nil
+  # uidを元にユーザーを検索または作成し、トークンがない場合や期限が切れている場合は更新
+  def self.from_omniauth(auth)
+    user = where(provider: auth.provider, uid: auth.uid).first_or_initialize do |u|
+      u.user_name = auth.info.name
+      u.email = auth.info.email if auth.info.email.present? # Twitchのemailがあれば保存
+      u.profile_image_url = auth.info.image
     end
+
+    # トークンがnilまたは期限が切れている場合に新しいトークンを取得して保存
+    if user.access_token.nil? || user.token_expires_at.nil? || user.token_expires_at < Time.now
+      Rails.logger.debug "トークンが無効または期限切れ、再取得を行います。"
+      user.access_token = auth.credentials.token
+      user.refresh_token = auth.credentials.refresh_token
+      user.token_expires_at = Time.at(auth.credentials.expires_at) if auth.credentials.expires
+    else
+      Rails.logger.debug "有効なアクセストークンが既に存在します。"
+    end
+
+    user.save! if user.changed? # 更新があれば保存
+    user
   end
 
-  def refresh_access_token(user)
-    return if user.refresh_token.blank? # リフレッシュトークンがない場合、再認証を要求
-    Rails.logger.debug "アクセストークンの再取得を試みます。"
+  # アクセストークンを再取得するロジック
+  def refresh_access_token
+    return if refresh_token.blank?
 
     response = Faraday.post("https://id.twitch.tv/oauth2/token") do |req|
       req.body = {
         client_id: ENV["TWITCH_CLIENT_ID"],
         client_secret: ENV["TWITCH_CLIENT_SECRET"],
-        refresh_token: user.refresh_token,
+        refresh_token: refresh_token,
         grant_type: "refresh_token"
       }
       req.headers["Content-Type"] = "application/x-www-form-urlencoded"
@@ -37,15 +40,14 @@ class UsersController < ApplicationController
 
     if response.success?
       token_data = JSON.parse(response.body)
-      user.update(
+      update(
         access_token: token_data["access_token"],
         refresh_token: token_data["refresh_token"],
         token_expires_at: Time.now + token_data["expires_in"].to_i.seconds
       )
-      Rails.logger.debug "アクセストークンの再取得に成功しました。"
+      Rails.logger.debug "アクセストークンの再取得に成功しました！"
     else
       Rails.logger.error "アクセストークンの再取得に失敗しました: #{response.body}"
-      redirect_to root_path, alert: "再認証が必要です。"
     end
   end
 end
