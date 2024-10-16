54d8d383164eb12a569de7379fe385c9ed552476
seki <102042736+HERUESTA@users.noreply.github.com>
2024-10-16 21:57:34 +0900

    リファクタリング前保存

--- app/controllers/streamers_controller.rb
+++ app/controllers/streamers_controller.rb
@@ -4,11 +4,13 @@ class StreamersController < ApplicationController
     streamer_info = get_streamer_info(streamer_name)
 
     if streamer_info
+      
       clips = fetch_clips(streamer_info[:id])
+      
 
       if clips.any?
         @clips = clips
-        @streamer_profile_image = streamer_info[:profile_image_url] # 配信者のプロフィール画像
+        @streamer_profile_image = streamer_info[:profile_image_url] 
         respond_to do |format|
           format.html do
             render partial: "streamers/clips", locals: { clips: @clips, streamer_profile_image: @streamer_profile_image, error_message: nil }
@@ -53,7 +55,7 @@ class StreamersController < ApplicationController
     if user_data["data"].any?
       {
         id: user_data["data"].first["id"],
-        profile_image_url: user_data["data"].first["profile_image_url"] # プロフィール画像のURLを取得
+        profile_image_url: user_data["data"].first["profile_image_url"] 
       }
     else
       nil
@@ -69,14 +71,12 @@ class StreamersController < ApplicationController
 
     clips = JSON.parse(response.body)["data"]
 
-    # プロフィール画像を含めるために配信者の情報を取得
     streamer_info = get_streamer_info_by_id(streamer_id)
 
     clips.select do |clip|
       clip["language"] == "ja"
     end.map do |clip|
-      game_name = fetch_game_name(clip["game_id"]) # ゲーム名を取得
-      # 配信者のプロフィール画像を追加
+      game_name = fetch_game_name(clip["game_id"]) 
       clip.merge("game_name" => game_name, "broadcaster_profile_image" => streamer_info[:profile_image_url])
     end
   end
@@ -98,7 +98,6 @@ class StreamersController < ApplicationController
     end
   end
 
-  # ゲームの名前を取得
   def fetch_game_name(game_id)
     access_token = ENV["TWITCH_ACCESS_TOKEN"]
     response = send_twitch_request("games", { id: game_id }, access_token)
@@ -106,17 +105,16 @@ class StreamersController < ApplicationController
     return nil unless response.success?
 
     game_data = JSON.parse(response.body)
-    game_data["data"].first["name"] if game_data["data"].any? # ゲーム名を取得
+    game_data["data"].first["name"] if game_data["data"].any? 
   end
 
-  # Twitch APIへのリクエスト送信
   def send_twitch_request(endpoint, params, access_token)
     Faraday.get("https://api.twitch.tv/helix/#{endpoint}") do |req|
       req.params = params
       req.headers["Authorization"] = "Bearer #{access_token}"
       req.headers["Client-ID"] = ENV["TWITCH_CLIENT_ID"]
-      req.options.timeout = 10  # タイムアウト設定 (秒)
-      req.options.open_timeout = 10  # 接続時のタイムアウト (秒)
+      req.options.timeout = 10  
+      req.options.open_timeout = 10 
     end
   end
 end
