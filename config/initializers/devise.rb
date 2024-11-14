82ded3a176eae4f4a403487393997ee9edcec5ca
seki <102042736+HERUESTA@users.noreply.github.com>
2024-11-14 21:54:37 +0900

    emailをuserテーブルから削除

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,11 +4,13 @@ Devise.setup do |config|
 
   require "devise/orm/active_record"
 
-  config.case_insensitive_keys = [ :email ]
-
-  config.strip_whitespace_keys = [ :email ]
-
   config.skip_session_storage = [ :http_auth ]
+
+  config.authentication_keys = [ :user_name ]
+
+  config.case_insensitive_keys = [ :user_name ]
+
+  config.strip_whitespace_keys = [ :user_name ]
 
   config.stretches = Rails.env.test? ? 1 : 12
 
@@ -26,7 +28,7 @@ Devise.setup do |config|
   config.responder.redirect_status = :see_other
 
   # OmniAuth設定
-  config.omniauth :twitch, ENV["TWITCH_CLIENT_ID"], ENV["TWITCH_CLIENT_SECRET"], scope: "user:read:email user:read:follows"
+  config.omniauth :twitch, ENV["TWITCH_CLIENT_ID"], ENV["TWITCH_CLIENT_SECRET"], scope: "user:read:follows"
 
   # OmniAuthでPOSTリクエストを許可
   OmniAuth.config.allowed_request_methods = [ :post ]
