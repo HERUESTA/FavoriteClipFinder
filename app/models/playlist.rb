70dc9e39e76fb6886b8ac3aaa6779da2734b77ac
seki <102042736+HERUESTA@users.noreply.github.com>
2024-11-16 16:28:50 +0900

    モデル構文の修正

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,7 +1,6 @@
 class Playlist < ApplicationRecord
-
   # 関連付け
-  belongs_to: user, foreign_key: 'user_uid'
+  belongs_to :user, foreign_key: 'user_uid'
 
   has_many :playlist_clips, dependent: :destroy
   has_many :clips, through: :playlist_clips
