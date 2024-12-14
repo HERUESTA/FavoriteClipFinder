417caa846be4058574dd88230ca23e78797f4b46
seki <102042736+HERUESTA@users.noreply.github.com>
2024-12-14 16:01:52 +0900

    マイグレーション修正

--- db/migrate/20241214061243_rename_streamers_to_broadcasters.rb
+++ db/migrate/20241214061243_rename_streamers_to_broadcasters.rb
@@ -1,5 +1,9 @@
 class RenameStreamersToBroadcasters < ActiveRecord::Migration[7.2]
   def change
-    rename_table :streamers, :broadcasters
+    if table_exists?(:streamers) && !table_exists?(:broadcasters)
+      rename_table :streamers, :broadcasters
+    else
+      puts "Skipping rename_table: 'streamers' does not exist or 'broadcasters' already exists."
+    end
   end
 end
