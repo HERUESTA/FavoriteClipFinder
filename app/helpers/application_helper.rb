237d8778fcd84ce0177ca6f5dfec57c9c347c89f
seki <102042736+HERUESTA@users.noreply.github.com>
2024-12-24 20:27:17 +0900

    X共有確認

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
   def default_meta_tags
+  playlist_title       = @playlist&.title.presence || "Twitchのクリップやプレイリストを共有できるサービス"
+  playlist_image_url   = @playlist&.clips&.first&.thumbnail_url || image_url("ogp.jpg")
+
     {
       site: "TwitchClipFinder",
       title: "Twitchのクリップやプレイリストを共有できるサービス",
@@ -18,28 +21,13 @@ module ApplicationHelper
         image: image_url("ogp.jpg"),
         locale: "ja-JP"
       },
+      # Twitterカード設定
       twitter: {
-        card: "summary_large_image",
-        site: "@obvyamdrss",
-        image: image_url("ogp.jpg")
-      }
+      card:        "summary_large_image",
+      site:        "@obvyamdrss",  # あなたのアカウント名に変更
+      title:       playlist_title,
+      image:       playlist_image_url
     }
-  end
-
-  def get_twitter_card_info(page)
-    twitter_card = {}
-    if page
-      twitter_card[:url] = page.url
-      twitter_card[:title] = @playlist.title
-      twitter_card[:image] = @playlist.clips.first&.thumbnail_url
-    else
-      twitter_card[:url] = playlist_path(@playlist)
-      twitter_card[:title] = "Webページ更新管理ツール「Moook」"
-      twitter_card[:description] = "いつもの更新いつもの更新確認、Moookを使えばお気に入りのページの更新を見逃しません。"
-      twitter_card[:image] = "https://raw.githubusercontent.com/Madogiwa0124/Moook/master/app/assets/images/favicon.png"
-    end
-    twitter_card[:card] = "summary"
-    twitter_card[:site] = "@Madogiwa_Boy"
-    twitter_card
+    }
   end
 end
