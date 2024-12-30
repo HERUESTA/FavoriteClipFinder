09b0cfab3b58aaa5fa26ec06be45a86b6a0fa9a4
seki <102042736+HERUESTA@users.noreply.github.com>
2024-12-31 02:00:01 +0900

    favoriteclipfinderの修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_twitch_clip_finder_session", same_site: :lax, secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: "_favorite_clip_finder_session", same_site: :lax, secure: Rails.env.production?
