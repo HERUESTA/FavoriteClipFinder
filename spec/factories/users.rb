06a62a8e60147a2285ee63f6d6d85fae90969aee
seki <102042736+HERUESTA@users.noreply.github.com>
2025-01-06 21:01:13 +0900

    ユーザースペックフォルダ作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,19 @@
 FactoryBot.define do
   factory :user do
+    sequence(:uid) { |n| "uid#{n}" }
+    sequence(:user_name) { |n| "user_name#{n}" }
+    profile_image_url { nil }
+    provider { nil }
+    access_token { nil }
+    refresh_token { nil }
+    token_expires_at { nil }
+    email { nil }
+  end
+
+  # Twitchユーザー
+  trait :twitch_user do
+    uid { "625219161" }
+    user_name { "siesta0905" }
+    profile_image_url
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#refresh_access_token' do
+    let(:user) { create(:user) }
+
+    context
+  end
 end
